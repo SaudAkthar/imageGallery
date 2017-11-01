f5f159c3731e2aebc13df5679a49e0faf183d7ca
saudakhtar <saudakhtars@cybage.com>
2017-11-01 11:16:32 +0530

    added auth service and two component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,48 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './guard/fake-backend';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { BaseRequestOptions } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
 
+import { routing } from './app.routing';
+
+import { AuthGuard } from './guard/auth.guard';
+import { UserService } from './services/user.service';
+import { AuthenticationService } from './services/authentication.service';
+
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
-  bootstrap: [ AppComponent ]
+  providers: [
+    AuthGuard,
+    AuthenticationService,
+    UserService,
+
+    // providers used to create fake backend
+    fakeBackendProvider,
+    MockBackend,
+    BaseRequestOptions
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
